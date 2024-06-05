73df4f74960007d97687335f70eb8bfbba11748b
vikasindoqubix11 <vikas.indoqubix@gmail.com>
2024-06-05 13:20:47 +0530

    Update Program.cs
    add port in the program .cs file to ensure it would be listen in the environment.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,37 @@
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+// builder.Services.AddRazorPages();
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
+
+// app.UseHttpsRedirection();
+// app.UseStaticFiles();
+
+// app.UseRouting();
+
+// app.UseAuthorization();
+
+// app.MapRazorPages();
+
+// app.Run();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Configure the web host to use the port defined by the PORT environment variable
+var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+builder.WebHost.UseUrls($"http://*:{port}");
 
 var app = builder.Build();
 
